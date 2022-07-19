33a008fd001042bf1368df2dcdcbf4474be25384
timf34 <farrelti@tcd.ie>
2022-07-19 23:19:06 +0100

    added **basic** wandb integration (no meaningful logging yet)

--- run_scripts/testing_supersuit.py
+++ run_scripts/testing_supersuit.py
@@ -5,10 +5,30 @@ from stable_baselines3 import PPO
 from pettingzoo.butterfly import pistonball_v6
 import supersuit as ss
 
+import wandb
+from wandb.integration.sb3 import WandbCallback
+
+
+# TODO: make this cleaner
+WANDB_API_KEY = '83230c40e1c562f3ef56bf082e31911eaaad4ed9'
+wandb.login(key=WANDB_API_KEY)
+
 # This is just a simple basic file to help ensure whether pettingzoo + supersuit + sb3 is working
 
 
 if __name__ == "__main__":
+
+    config = {
+        "total_timesteps": 20000
+       , "env_name": "pistonball_v6"
+    }
+
+    run = wandb.init(
+        project="sb3-testing",
+        config=config,
+        sync_tensorboard=True,  # auto-upload sb3's tensorboard metrics
+        save_code=True
+    )
 
     env = pistonball_v6.parallel_env()
     print("1. Env type is: ", type(env))
@@ -24,4 +44,9 @@ if __name__ == "__main__":
     print("6. Env type is: ", type(env))
     model = PPO('CnnPolicy', env, verbose=3, n_steps=16)
     print("We made it")
-    model.learn(total_timesteps=2000000)
+    model.learn(total_timesteps=20000,
+                callback=WandbCallback(
+                    gradient_save_freq=100,
+                    verbose=2)
+                )
+    run.finish()
