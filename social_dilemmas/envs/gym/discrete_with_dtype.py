a463d5695376e5ea4c3e1f3cb714d3977249cb73
timf34 <farrelti@tcd.ie>
2022-07-19 11:01:20 +0100

    added `start` attribute and basic test

--- social_dilemmas/envs/gym/discrete_with_dtype.py
+++ social_dilemmas/envs/gym/discrete_with_dtype.py
@@ -1,10 +1,25 @@
+import numpy as np
 from gym.spaces import Discrete
 
 # TODO: this could probably be moved to a utils file.
 
 class DiscreteWithDType(Discrete):
-    def __init__(self, n, dtype):
+    def __init__(self, n, dtype, start: int = 0):
         assert n >= 0
         self.n = n
+        self.start = start
         # Skip Discrete __init__ on purpose, to avoid setting the wrong dtype
         super(Discrete, self).__init__((), dtype)
+
+
+def test_discrete_with_d_type():
+    x = DiscreteWithDType(8, dtype=np.uint8)
+
+    print("vars(x):", vars(x))
+    assert 'start' in vars(x)
+
+
+if __name__ == "__main__":
+    test_discrete_with_d_type()
+
+
