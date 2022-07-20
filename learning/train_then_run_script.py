9be9e4b38db3cace308d55e037005d0e88a3d689
timf34 <farrelti@tcd.ie>
2022-07-20 16:19:01 +0100

    initial commit

---
+++ learning/train_then_run_script.py
@@ -0,0 +1,22 @@
+import gym
+
+from stable_baselines3 import A2C
+
+env = gym.make('CartPole-v1')
+
+model = A2C('MlpPolicy', env, verbose=1)
+model.learn(total_timesteps=10000)
+
+# Note that this piece of code doesn't work if we run it through WSL as it can't connect to the screen via .render()
+
+obs = env.reset()
+for i in range(1000):
+    action, _state = model.predict(obs, deterministic=True)
+    obs, reward, done, info = env.step(action)
+    env.render()
+    if done:
+      obs = env.reset()
+
+env.close()
+
+
