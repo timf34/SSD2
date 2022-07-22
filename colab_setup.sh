acdf37a0996d3e10767011205537ab705054beb4
timf34 <farrelti@tcd.ie>
2022-07-22 23:52:56 +0100

    commented out commands to work with a fresh colab

--- colab_setup.sh
+++ colab_setup.sh
@@ -4,9 +4,11 @@
 
 # Hardcoded path
 # This could be reduced to one line but I know this works
-%cd ~
-%cd ..
-%cd content/gdrive/MyDrive/'Personal - 3rd year'/CERI/'Christian, Tim and Timothy '/ColabDirs/SSD2
+
+# Also assuming that we just clone the repo fresh each time - that way its easier to git pull I think, etc.
+#%cd ~
+#%cd ..
+#%cd content/gdrive/MyDrive/'Personal - 3rd year'/CERI/'Christian, Tim and Timothy '/ColabDirs/SSD2
 
 # Assuming that we are using Python 3.7
 # We will first create our virtual environment if it has not been made already
