13d795fa538122b2e56ab620262393dbeabc2280
timf34 <farrelti@tcd.ie>
2022-08-03 23:42:05 +0100

    changed to harvest

--- config/configuration.py
+++ config/configuration.py
@@ -7,18 +7,18 @@ import time
 @dataclass
 class Config:
     # Parser args
-    env_name: str = "cleanup" # Choices: ["harvest", "cleanup"]
-    num_agents: int = 5
+    env_name: str = "harvest" # Choices: ["harvest", "cleanup"]
+    num_agents: int = 1
     rollout_len: int = 1000
-    total_timesteps: int = 1e8
+    total_timesteps: int = 2e8
     use_collective_reward: bool = False # Give each agent the collective reward across all agents
     inequity_averse_reward: bool = False # Use inequity averse rewards from 'Inequity aversion...' paper
     alpha: float = 5
     beta: float = 0.05
 
     # Args from def main():
-    num_cpus: int = 6
-    num_envs: int = 6  # Number of parallel multi-agent environments
+    num_cpus: int = 12
+    num_envs: int = 8  # Number of parallel multi-agent environments
     num_frames: int = 8  # Number of frames to stack together for input to the network; use >4 to avoid automatic VecTransposeImage
     features_dim: int= 128  # output layer of cnn extractor AND shared layer for policy and value functions
     fcnet_hiddens: Tuple[int, int] = (1024, 128)  # Two hidden layers for cnn extractor
@@ -40,7 +40,7 @@ class Config:
     vec_video_rollout_legnth: int = 1000 # How many steps to save to the video (default is 200, and total episode would be 1000)
 
     def __post_init__(self):
-        self.wandb_experiment_name: str = f"PPO_ONE_AGENT_{time.strftime('%d_%m_%Y_%H%M%S')}"
+        self.wandb_experiment_name: str = f"PPO_ONE_AGENT_HARVEST_{time.strftime('%d_%m_%Y_%H%M%S')}"
 
 
 def test_config():
