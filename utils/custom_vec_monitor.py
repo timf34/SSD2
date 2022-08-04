349ac12bb0a661b2dd9a7c8c4629ee6605a080a7
timf34 <farrelti@tcd.ie>
2022-08-04 15:38:49 +0100

    added txt files for custom_vec_monitor.py

--- utils/custom_vec_monitor.py
+++ utils/custom_vec_monitor.py
@@ -2,7 +2,9 @@ import time
 from typing import Optional, Tuple, Dict, List
 from stable_baselines3.common.vec_env.vec_monitor import VecMonitor
 from stable_baselines3.common.vec_env.base_vec_env import VecEnvStepReturn, VecEnv
+from stable_baselines3 import PPO
 
+from utils.sb3_custom_cnn import CustomCNN
 from utils.env_getter_utils import get_supersuit_parallelized_environment
 from config.configuration import Config
 
@@ -24,9 +26,7 @@ class CustomVecMonitor(VecMonitor):
         if self.number_agents is not None:
             for i in range(self.number_agents):
                 agent_id = f"agent-{str(i)}"
-                pass
                 # self.agents
-
 
     def print_venv_attributes(self):
         print("\n Here are the attributes of the venv: ")
@@ -64,59 +64,6 @@ class CustomVecMonitor(VecMonitor):
                     self.results_writer.write_row(episode_info)
                 new_infos[i] = info
 
-
-
-        # Shape: (number of agents (=number of agents in env x number of environments), 15, 15, 18)
-        # The latter dimension is 6 (the number of frames being stacked before input to the network) x 3 (the number of channels)
-        # I'll test this in a sec.
-        print("Here are the obs shape: ")
-        print(obs.shape)
-
-        # Sample: [ -28. -327. -378. -442. -175. -381. -327.  -34.  -68. -318.] -> List[int]
-        print("self.episode_returns: ")
-        print(self.episode_returns)
-
-        # [  0.  -1.  -1.   0.   0.   0. -50.   1.   0.  -1.] -> List[int]
-        # -50 means the agent got hit, -1 means they zapped a beam, +1 means they ate an apple.
-        print("\n Here are the rewards: ")
-        print(rewards)
-
-        # Essentially a bool for when the episode is done for each agent
-        # [0 0 0 0 0 0 0 0 0 0] -> List[int] (0 means not done, 1 means done)
-        print("\n Here are the dones: ")
-        print(dones)
-
-        # An empty tuple it seems.
-        print("\n info key words:")
-        print(self.info_keywords)
-
-
-        # [{'terminal_observation': array([[[  0, 255,   0, ...,   0,   0,   0],
-        #         [113,  75,  24, ..., 113,  75,  24],
-        print("\n Here are the infos: ")
-        print(infos)
-
-        # During an episode
-        # {}
-
-        # At the end of an episode
-        # {'r': -292.0, 'l': 1000, 't': 59.348197}
-        print("episode info:")
-        print(episode_info)
-
-
-        # When done is False (during an episode
-        #  [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}]
-
-        # At the end of an episode (ie when done is True)
-        # [{'terminal_observation': array([[[0, 255, 0, ..., 0, 0, 0],
-        #                                  [113, 75, 24, ..., 113, 75, 24],
-        #                                  [0, 255, 0, ..., 0, 0, 0],
-        #                                  ...,
-        # [  0,   0,   0, ...,   0,   0,   0]]], dtype=uint8), 'episode': {'r': -91.0, 'l': 1000, 't': 59.345771}}, {'terminal_observation': array([[[  0,   0,   0, ...,   0,   0,   0],
-        print("\n Here are the new_infos: ")
-        print(new_infos)
-
         return obs, rewards, dones, new_infos
 
 
@@ -129,15 +76,32 @@ def test_taking_metrics():
 
     pass
 
-def test_vec_monitor():
+
+def test_vec_monitor(train=True):
     """
-    We will just use this to test that we are taking metrics, etc. properly.
-    Using inputs such as we have above.
+    x.
     :return:
     """
     args = Config()
-    env = get_supersuit_parallelized_environment()
+    env = get_supersuit_parallelized_environment(args)
     env = CustomVecMonitor(env, filename=args.log_file_path)
 
+    if train:
+        tensorboard_log = f"./logs/tb_results/sb3/{args.env_name}_ppo_paramsharing"
+
+        policy_kwargs = dict(
+            features_extractor_class=CustomCNN,
+            features_extractor_kwargs=dict(
+                features_dim=args.features_dim, num_frames=args.num_frames, fcnet_hiddens=args.fcnet_hiddens),
+            net_arch=[args.features_dim])
+
+        model = PPO(
+            "CnnPolicy", env=env,learning_rate=args.lr, n_steps=args.rollout_len, batch_size=args.batch_size,
+            n_epochs=args.n_epochs, gamma=args.gamma, gae_lambda=args.gae_lambda, ent_coef=args.ent_coef,
+            max_grad_norm=args.grad_clip, target_kl=args.target_kl, policy_kwargs=policy_kwargs,
+            tensorboard_log=tensorboard_log, verbose=args.verbose, device='cuda'
+        ).learn(total_timesteps=args.total_timesteps)
+
+
 if __name__ == '__main__':
-    pass
+    test_vec_monitor()
