19e9030a3ced288f1607caf69d913eedb77ed906
timf34 <farrelti@tcd.ie>
2022-08-04 18:39:42 +0100

    added array slicing

---
+++ learning/array_slicing.py
@@ -0,0 +1,23 @@
+sample_array = [i for i in range(10)]
+print(sample_array)
+
+step_size = len(sample_array) // 2
+print("step size", step_size)
+
+# Now I want to loop through the whole array with a step size of 5 (i.e. every 5th element)
+for i in range(step_size):
+    for j in range(i, len(sample_array), step_size):
+        # print the value
+        print(sample_array[j])
+
+# Now I want to loop through the array again, with a step size of 5, but to visit each element using slicing
+# This is the one:)
+for i in range(step_size):
+    print(sample_array[i::step_size])
+
+# Now to repeat with a better sample array
+sample_rewards = [0., -1.,  1.,  0., -1.,  0.,  -50., -1., 0, 0]
+
+for i in range(step_size):
+    print(sample_rewards[i::step_size])
+
