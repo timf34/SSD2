4cc93c24bde1f6f335cdad036c9eaf08a0a9c738
timf34 <farrelti@tcd.ie>
2022-08-04 13:42:18 +0100

    moved CustomCNN to utils dir

--- sb3_train.py
+++ sb3_train.py
@@ -1,22 +1,20 @@
 import os
 import datetime
 
-import gym
 import supersuit as ss
 import torch
-import torch.nn.functional as F
 from stable_baselines3 import PPO
-from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
 from stable_baselines3.common.vec_env.vec_monitor import VecMonitor
-from torch import nn
 import wandb
 from wandb.integration.sb3 import WandbCallback
 
-from utils.wandb_vec_vid_recorder import WandbVecVideoRecorder
 from social_dilemmas.envs.pettingzoo_env import parallel_env
 from config.configuration import Config
 from utils.custom_vec_monitor import CustomVecMonitor
 from utils.env_getter_utils import get_supersuit_parallelized_environment
+from utils.sb3_custom_cnn import CustomCNN
+from utils.wandb_vec_vid_recorder import WandbVecVideoRecorder
+
 
 
 DEVICE = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
@@ -33,45 +31,6 @@ datetime_filename = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 os.makedirs(LOG_DIR, exist_ok=True)
 log_file_path = os.path.join(LOG_DIR, f"{datetime_filename}")
 video_file_path = os.path.join(VIDEO_DIR, f"{datetime_filename}")
-
-
-# Use this with lambda wrapper returning observations only
-class CustomCNN(BaseFeaturesExtractor):
-    """
-    :param observation_space: (gym.Space)
-    :param features_dim: (int) Number of features extracted.
-        This corresponds to the number of unit for the last layer.
-    """
-    def __init__(
-        self,
-        observation_space: gym.spaces.Box,
-        features_dim=128,
-        view_len=7,
-        num_frames=6,
-        fcnet_hiddens=[1024, 128],
-    ):
-        super(CustomCNN, self).__init__(observation_space, features_dim)
-        # We assume CxHxW images (channels first)
-        # Re-ordering will be done by pre-preprocessing or wrapper
-
-        flat_out = num_frames * 6 * (view_len * 2 - 1) ** 2
-        self.conv = nn.Conv2d(
-            in_channels=num_frames * 3,  # Input: (3 * 4) x 15 x 15
-            out_channels=num_frames * 6,  # Output: 24 x 13 x 13
-            kernel_size=3,
-            stride=1,
-            padding="valid",
-        )
-        self.fc1 = nn.Linear(in_features=flat_out, out_features=fcnet_hiddens[0])
-        self.fc2 = nn.Linear(in_features=fcnet_hiddens[0], out_features=fcnet_hiddens[1])
-
-    def forward(self, observations) -> torch.Tensor:
-        # Convert to tensor, rescale to [0, 1], and convert from B x H x W x C to B x C x H x W
-        observations = observations.permute(0, 3, 1, 2)
-        features = torch.flatten(F.relu(self.conv(observations)), start_dim=1)
-        features = F.relu(self.fc1(features))
-        features = F.relu(self.fc2(features))
-        return features
 
 
 def main(args):
---
+++ utils/sb3_custom_cnn.py
@@ -0,0 +1,44 @@
+import gym
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
+
+
+# Use this with lambda wrapper returning observations only
+class CustomCNN(BaseFeaturesExtractor):
+    """
+    :param observation_space: (gym.Space)
+    :param features_dim: (int) Number of features extracted.
+        This corresponds to the number of unit for the last layer.
+    """
+    def __init__(
+        self,
+        observation_space: gym.spaces.Box,
+        features_dim=128,
+        view_len=7,
+        num_frames=6,
+        fcnet_hiddens=[1024, 128],
+    ):
+        super(CustomCNN, self).__init__(observation_space, features_dim)
+        # We assume CxHxW images (channels first)
+        # Re-ordering will be done by pre-preprocessing or wrapper
+
+        flat_out = num_frames * 6 * (view_len * 2 - 1) ** 2
+        self.conv = nn.Conv2d(
+            in_channels=num_frames * 3,  # Input: (3 * 4) x 15 x 15
+            out_channels=num_frames * 6,  # Output: 24 x 13 x 13
+            kernel_size=3,
+            stride=1,
+            padding="valid",
+        )
+        self.fc1 = nn.Linear(in_features=flat_out, out_features=fcnet_hiddens[0])
+        self.fc2 = nn.Linear(in_features=fcnet_hiddens[0], out_features=fcnet_hiddens[1])
+
+    def forward(self, observations) -> torch.Tensor:
+        # Convert to tensor, rescale to [0, 1], and convert from B x H x W x C to B x C x H x W
+        observations = observations.permute(0, 3, 1, 2)
+        features = torch.flatten(F.relu(self.conv(observations)), start_dim=1)
+        features = F.relu(self.fc1(features))
+        features = F.relu(self.fc2(features))
+        return features
