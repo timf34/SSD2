7966a56057e3fa75a556b515b18144a91c9f0194
timf34 <farrelti@tcd.ie>
2022-08-10 17:27:11 +0100

    prior to refactor

--- utils/custom_vec_monitor.py
+++ utils/custom_vec_monitor.py
@@ -8,8 +8,24 @@ from utils.sb3_custom_cnn import CustomCNN
 from utils.env_getter_utils import get_supersuit_parallelized_environment
 from config.configuration import Config
 
+# TODO: note down this crazy silly error. This specific individual dict was being shared! I need to use objects here!
+agent_dict = {"individual_rewards": [], "beam_fired": [], "beam_hit": [], "apples_consumed": []}
 
-agent_dict = {"indivudal_rewards": [], "beam_fired": [], "beam_hit": [], "apples_consumed": []}
+
+class Agent:
+    def __init__(self, agent_id):
+        self.agent_id = agent_id
+        self.individual_rewards: List[float] = []
+        self.beam_fired: List[int] = []
+        self.beam_hit: List[int] = []
+        self.apples_consumed: List[int] = []
+
+    # The below two methods are for item assignment (calling and setting things like with a dict -> dict["key"] = value)
+    def __setitem__(self, key, value):
+        setattr(self, key, value)
+
+    def __getitem__(self, key):
+        return getattr(self, key)
 
 
 class CustomVecMonitor(VecMonitor):
@@ -28,7 +44,7 @@ class CustomVecMonitor(VecMonitor):
             for i in range(self.number_agents):
                 agent_id = f"agent-{str(i)}"
                 # TODO: We might want to use a class here, then I could also use a .reset() method
-                self.agents[agent_id] = agent_dict
+                self.agents[agent_id] = Agent(agent_id)
 
     def print_venv_attributes(self):
         print("\n Here are the attributes of the venv: ")
@@ -45,6 +61,8 @@ class CustomVecMonitor(VecMonitor):
             print(f"{key}: {value}")
 
     def step_wait(self) -> VecEnvStepReturn:
+
+        # TODO: note that everything done here is done every step!
         obs, rewards, dones, infos = self.venv.step_wait()
         self.episode_returns += rewards
         self.episode_lengths += 1
@@ -52,6 +70,7 @@ class CustomVecMonitor(VecMonitor):
         episode_info = {}
         for i in range(len(dones)):
             if dones[i]:
+                # TODO: note that everything done here is done only at the end of an episode!
                 info = infos[i].copy()
                 episode_return = self.episode_returns[i]
                 episode_length = self.episode_lengths[i]
@@ -85,40 +104,38 @@ def pseudo_step(rewards, dones, step_size=5) -> None:
     print(rewards)  # TODO: why don't the rewards match the individual agent rewards.
     for i in range(5):
         agent_id = f"agent-{str(i)}"
-
-        agents[agent_id] = agent_dict
+        agents[agent_id] = Agent(agent_id)
 
     for i in range(step_size):
         for j in range(i, len(dones), step_size):
             print(f"{i} {j}")
-            print(f"{rewards[j]} {dones[j]}")
+            print(f"rewards[i] and dones[j] {rewards[j]} {dones[j]}")
             agent_id = f"agent-{str(i)}"
             if dones[j]:
                 print(agent_id)
-                print(agents[agent_id])
 
                 # Individual agent metrics.
                 # Note that this wouldn't include metrics from the last step!
                 # agent_id = j % 5
                 print(rewards)
                 print("agents[agent_id]", agents[agent_id])
-                agents[f"{agent_id}"]["indivudal_rewards"] += [rewards[i]]
+                agents[f"{agent_id}"]["individual_rewards"] += [rewards[i]]
                 if rewards[i] == -1:
                     agents[f"{str(agent_id)}"]["beam_fired"] += [1]
                 elif rewards[i] == 1:
                     agents[f"{str(agent_id)}"]["apples_consumed"] += [1]
                 elif rewards[i] == -50:
-                    agents[f"agent-{str(agent_id)}"]["beam_hit"] += [1]
+                    agents[f"{str(agent_id)}"]["beam_hit"] += [1]
+
 
     # print the agents dict
-    for agent_id, _agent_dict in agents.items():
-        print(f"\nAgent {agent_id}")
-        for key, value in _agent_dict.items():
-            print(f"{key}: {value}")
+    for agent_id, agent_object in agents.items():
+        print(f"\nAgent {agent_object.__dict__}")
 
     print(agents)
 
-def test_taking_metrics():
+
+def lets_tests_taking_metrics():
     """
     We will just use this to test that we are taking metrics, etc. properly.
     Using inputs such as we have above.
@@ -130,7 +147,7 @@ def test_taking_metrics():
     pseudo_step(sample_rewards, dones)
 
 
-def test_vec_monitor(train=True):
+def lets_tests_vec_monitor(train=True):
     """
     x.
     :return:
@@ -159,4 +176,4 @@ def test_vec_monitor(train=True):
 
 if __name__ == '__main__':
     # test_vec_monitor()
-    test_taking_metrics()
+    lets_tests_taking_metrics()
