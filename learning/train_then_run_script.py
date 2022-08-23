901bc8be578312d6cb9f083932765fce6e980e11
timf34 <farrelti@tcd.ie>
2022-08-23 12:19:25 +0100

    reduced training time

--- learning/train_then_run_script.py
+++ learning/train_then_run_script.py
@@ -5,7 +5,7 @@ from stable_baselines3 import A2C
 env = gym.make('CartPole-v1')
 
 model = A2C('MlpPolicy', env, verbose=1)
-model.learn(total_timesteps=10000)
+model.learn(total_timesteps=5000)
 
 # Note that this piece of code doesn't work if we run it through WSL as it can't connect to the screen via .render()
 
