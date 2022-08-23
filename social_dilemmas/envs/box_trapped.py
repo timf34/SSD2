18ec408f114c7ca9fb89e829d0e7b05105867c98
timf34 <farrelti@tcd.ie>
2022-08-23 01:57:38 +0100

    initial commit of box trapped (not finished)

---
+++ social_dilemmas/envs/box_trapped.py
@@ -0,0 +1,91 @@
+import numpy as np
+from numpy.random import rand
+
+from social_dilemmas.envs.agent import BoxTrappedAgent
+from social_dilemmas.envs.gym.discrete_with_dtype import DiscreteWithDType
+from social_dilemmas.envs.map_env import MapEnv
+from social_dilemmas.maps import BOX_TRAPPED_MAP
+
+# For consistency with the other environments, I'll also use "FIRE" for now, but we can probs remove it in this env tbh
+_BOX_TRAPPED_ACTIONS = {"FIRE": 5, "UNLOCK": 1}  # length of unlocking beam (we won't actually see it anyways)
+BOX_TRAPPED_VIEW_SIZE = 7
+
+
+class BoxTrapped(MapEnv):
+    def __init__(
+            self,
+            ascii_map=BOX_TRAPPED_MAP,
+            num_agents=2,
+            return_agent_actions=True,
+            use_collective_reward=False,
+            inequity_averse_reward=False,
+            alpha=0.0,
+            beta=0.0,
+    ):
+        super().__init__(
+            ascii_map=ascii_map,
+            extra_actions=_BOX_TRAPPED_ACTIONS,
+            view_len=BOX_TRAPPED_VIEW_SIZE,
+            num_agents=num_agents,
+            return_agent_actions=return_agent_actions,
+            use_collective_reward=use_collective_reward,
+            inequity_averse_reward=inequity_averse_reward,
+            beta=beta,
+            alpha=alpha,
+        )
+        self.apple_points = []
+        self.box_is_locked: bool = True
+        for row in range(self.base_map.shape[0]):
+            for col in range(self.base_map.shape[1]):
+                if self.base_map[row, col] == b"A":
+                    self.apple_points.append([row, col])
+
+    @property
+    def action_space(self):
+        return DiscreteWithDType(9, dtype=np.int8)
+
+    def custom_action(self, agent, action):
+        updates = []
+        if action == "FIRE":
+            agent.fire_beam(b"F")
+            updates = self.update_map_fire(
+                agent.pos.tolist(),
+                agent.get_orientation(),
+                self.all_actions["FIRE"],
+                fire_char=b"F",
+            )
+        elif action == "UNLOCK":
+            pass
+        return updates
+
+    def custom_reset(self):
+        """Initialize the walls, the apples and lock the box."""
+
+        self.box_is_locked = True
+
+        # TODO: ensure that the agent spawns in the correct place.
+        for apple_point in self.apple_points:
+            self.single_update_map(apple_point[0], apple_point[1], b"A")
+
+
+    def setup_agents(self):
+        # Initializing the agents.
+        map_with_agents = self.get_map_with_agents()
+
+        for i in range(self._num_agents):
+            agent_id = f"agent-{str(i)}"
+            spawn_point = self.spawn_point()
+            rotation = self.spawn_rotation()
+            grid = map_with_agents
+            agent = BoxTrappedAgent(agent_id, spawn_point, rotation, grid, view_len=BOX_TRAPPED_VIEW_SIZE)
+            self.agents[agent_id] = agent
+
+    def custom_map_update(self):
+        # TODO: oh wait is this the right thing.. its in step func.
+        # This is just an update that happens every step!
+        # Spawns the apples
+        # Sets the Box back to Locked state
+        new_apples = self.spawn_apples()  # CHECK THE REGROW RATE ON THIS! I want it to be basically instant!
+        self.update_map(new_apples)
+        # Lock the box again.
+        self.box_is_locked = True
