a2b4a5bda00f33d3bb8f012f31d0872520c20e2f
timf34 <farrelti@tcd.ie>
2022-08-29 00:42:17 +0100

    wandb integrated and checkpoint config

--- ray_train.py
+++ ray_train.py
@@ -1,15 +1,71 @@
+import ray
+from ray import air, tune
 from ray.rllib.models import ModelCatalog
 from ray.tune.registry import register_env
 from ray.rllib.algorithms.a2c import A2CConfig, A2C
+from ray.rllib.algorithms.a3c import A3CConfig, A3C
 from ray.tune.logger import pretty_print
+from ray.tune.integration.wandb import WandbLoggerCallback
+
+from typing import Dict
+import wandb
 
 from social_dilemmas.config.default_args import add_default_args
 from social_dilemmas.envs.env_creator import get_env_creator
 
 from config.configuration import Config
 
+USE_TUNE = True
 
-def build_config(args: Config):
+WANDB_API_KEY = '83230c40e1c562f3ef56bf082e31911eaaad4ed9'
+wandb.login(key=WANDB_API_KEY)
+
+def configure_model(config: Dict) -> Dict:
+    """
+    configures the model - this function is largely just to remove config code from the main trianing script
+    :param config:
+    :return:
+    """
+    config["model"] = {}
+    # The final layer must be chosen specifically so that its output is [B, 1, 1, X]. See the explanation in
+    # https://docs.ray.io/en/latest/rllib-models.html#built-in-models.
+    # It is because rllib is unable to flatten to a vector otherwise.
+    config["model"] = {"conv_filters": [[6, [3, 3], 1]],
+                       "conv_activation": "relu",
+                       "fcnet_hiddens": [32, 32],
+                       "use_lstm": True,
+                       "lstm_use_prev_action": True,
+                       "lstm_use_prev_reward": False,
+                       "lstm_cell_size": 128}
+    return config
+
+
+def configure_config(env_creator, args, policy_graphs, policy_mapping_fn) -> Dict:
+    config = A3CConfig().to_dict()
+
+    config["num_workers"] = 1  # args.num_cpus
+    config["framework"] = "torch"
+    config["env"] = args.env_name
+
+    # information for replay - not sure why we need this
+    config["env_config"]["func_create"] = env_creator
+    config["env_config"]["env_name"] = args.env_name
+
+    # Set the num_gpus to 0
+    config["num_gpus"] = 0
+
+    # configure model
+    config = configure_model(config)
+
+    # Multi-agents
+    config["multiagent"] = {"policies": policy_graphs, "policy_mapping_fn": policy_mapping_fn}
+
+    # hyperparams
+    config["horizon"] = 1000
+    return config
+
+
+def training_script(args: Config):
 
     env_creator = get_env_creator(env=args.env_name,
                                   num_agents=args.num_agents,
@@ -20,9 +76,8 @@ def build_config(args: Config):
     # Get the observation and action space sizes
     single_env = env_creator(args.num_agents)
     obs_space = single_env.observation_space
-    # print("obs spcaes: ", obs_space)
     act_space = single_env.action_space
-    # print("act spcaes: ", act_space)
+    # print("obs spcaes: ", obs_space, "act spcaes: ", act_space)
 
     # Agent policies initialized
     def policy_mapping_fn(agent_id):
@@ -35,61 +90,59 @@ def build_config(args: Config):
     # Create 1 distinct policy per agent
     policy_graphs = {}
     for i in range(args.num_agents):
-        policy_graphs["agent-" + str(i)] = gen_policy()
+        policy_graphs[f"agent-{str(i)}"] = gen_policy()
 
-    config = A2CConfig()
+    config = configure_config(env_creator, args, policy_graphs, policy_mapping_fn)
 
+    stop = {
+        "training_iteration": 10000,
+        "timesteps_total" : 2e8
+    }
 
-    config.num_workers = 1  # args.num_cpus
-    config.framework_str = "torch"
-    print(config.to_dict())
-    config = config.to_dict()
+    algo = A3C(env=args.env_name, config=config)
 
-    # information for replay - not sure why we need this
-    config["env_config"]["func_create"] = env_creator
-    config["env_config"]["env_name"] = args.env_name
+    print(config)
 
-    # Set the num_gpus to 1
-    config["num_gpus"] = 1
+    if not USE_TUNE:
 
-    # TODO :see why the conifg for the CNN from MeltingPot didn't work.
+        # Updated config file passed to the algorithm (trainer... actually not sure if its entirely equivalen)
 
-    # Setup for the neural network.
-    config["model"] = {}
-    # The strides of the first convolutional layer were chosen to perfectly line
-    # up with the sprites, which are 8x8.
-    # The final layer must be chosen specifically so that its output is
-    # [B, 1, 1, X]. See the explanation in
-    # https://docs.ray.io/en/latest/rllib-models.html#built-in-models. It is
-    # because rllib is unable to flatten to a vector otherwise.
-    # The a3c models used as baselines in the meltingpot paper were not run using
-    # rllib, so they used a different configuration for the second convolutional
-    # layer. It was 32 channels, [4, 4] kernel shape, and stride = 1.
-    config["model"]["conv_filters"] = [[6, [3, 3], 1]]
-    config["model"]["conv_activation"] = "relu"
-    config["model"]["fcnet_hiddens"] = [32, 32]
-    config["model"]["use_lstm"] = True
-    config["model"]["lstm_use_prev_action"] = True
-    config["model"]["lstm_use_prev_reward"] = False
-    config["model"]["lstm_cell_size"] = 128
+        # Is this even enough? This would be 1000 episodes I believe
+        for i in range(1000):
+            # Perform one iteration of training the policy with PPO
+            print("here is i", i)
+            result = algo.train()
+            print(pretty_print(result))
 
-    # hyperparams
-    config["multiagent"] = {"policies": policy_graphs, "policy_mapping_fn": policy_mapping_fn}
-    config["horizon"] = 1000
-
-    # Updated config file passed to the trainer.
-    # This is equivalen to `get_trainer`
-    algo = A2C(env=args.env_name, config=config)
-
-    for i in range(1000):
-        # Perform one iteration of training the policy with PPO
-        result = algo.train()
-        print(pretty_print(result))
+            if i % 100 == 0:
+                checkpoint = algo.save()
+                print("checkpoint saved at", checkpoint)
+    else:
+        # ray.init(local_mode=True)
+        # ray.init()
+        tuner = ray.tune.Tuner(
+            trainable="A3C",
+            param_space=config,
+            run_config=air.RunConfig(name="testingA3C",
+                                     stop=stop,
+                                     callbacks=[
+                                        WandbLoggerCallback(api_key=WANDB_API_KEY,
+                                                            project="Ray Testing",
+                                                            log_config=False,
+                                                            save_checkpoints=True)
+                                     ],
+                                     local_dir="tmp/mypath",
+                                     checkpoint_config=air.CheckpointConfig(num_to_keep=100,
+                                                                            checkpoint_frequency=1,
+                                                                            checkpoint_at_end=True)
+            )
+        )
+        tuner.fit()
 
 
 def main():
     args = Config()
-    build_config(args)
+    training_script(args)
 
 
 if __name__ == "__main__":
